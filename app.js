1f2d72a5f33c1b28acb05dc614ba8a8760a7185c
RSamokhin <poshliemail@googlemail.com>
2016-10-22 02:51:49 +0300

    web

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ var koa = require('koa'),
     serveStatic = require('koa-static'),
     app = module.exports = koa();
 
-app.use(serveStatic(__dirname + '/web/public/'));
+app.use(serveStatic(__dirname + './../web/'));
 
 app.listen(3000);
 console.log('server is running');
